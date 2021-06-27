b896677b6ea42cd226435e2ad251702f0c6048e9
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2021-06-27 16:49:04 +0530

    Update start.py

--- PicBot/Plugins/start.py
+++ PicBot/Plugins/start.py
@@ -1,11 +1,16 @@
-from pyrogram import Client, filters 
+from pyrogram import Client, filters
 
 
 @Client.on_message(filters.command(["start"]))
 async def start(bot, update):
     me = client.me.first_name
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=f"__Hello {me}!\nI'm Picbot\nCheck__ /help __to know how to use me\n**My Owner:** [Naveen](t.me/Sniper_xd)",
-        reply_to_message_id=update.message_id
-    )
+    text=f"__Hello {me}!\nI'm Picbot\nCheck__ /help __to know how to use me\n**My Owner:** [Naveen](t.me/Sniper_xd)"
+    mypic="https://telegra.ph//file/894df85d26b8b72f0745b.jpg"
+    await bot.send_photo(
+            message.chat.id,
+            mypic,
+            text,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("Dev", url="t.me/Sniper_xd")]]
+            ),
+        )
