2d45524cc30c5a299280d19746a887e06373d00b
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2021-06-27 16:50:48 +0530

    Update start.py

--- PicBot/Plugins/start.py
+++ PicBot/Plugins/start.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 @Client.on_message(filters.command(["start"]))
 async def start(bot, update):
     me = client.me.first_name
-    text=f"__Hello {me}!\nI'm Picbot\nCheck__ /help __to know how to use me\n**My Owner:** [Naveen](t.me/Sniper_xd)"
+    text=f"__Hello {me}!\nI'm Picturebot\nCheck__/help__ to get available commands"
     mypic="https://telegra.ph//file/894df85d26b8b72f0745b.jpg"
     await bot.send_photo(
             message.chat.id,
