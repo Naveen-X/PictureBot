2e99df01f2f1ccc0f9f3c64ec42205fba5f5dee3
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2021-06-27 20:23:43 +0530

    Update animal_pic.py

--- PicBot/Plugins/animal_pic.py
+++ PicBot/Plugins/animal_pic.py
@@ -8,3 +8,19 @@ async def panda(bot, update):
     r = requests.get(url=link).json()
     image_s = r["image"]
     await bot.send_photo(image_s)
+
+
+@Client.on_message(filters.command(["cat"]))
+async def cat(bot, update):
+    link = "https://some-random-api.ml/img/cat"
+    r = requests.get(url=link).json()
+    image_s = r["image"]
+    await bot.send_photo(image_s)
+
+
+@Client.on_message(filters.command(["dog"]))
+async def dog(bot, update):
+    link = "https://some-random-api.ml/img/dog"
+    r = requests.get(url=link).json()
+    image_s = r["image"]
+    await bot.send_photo(image_s)
