7027fcf85a51344d26f8744461b00d22a29c784d
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2021-06-29 15:23:36 +0530

    Update help.py

--- PicBot/Plugins/help.py
+++ PicBot/Plugins/help.py
@@ -2,5 +2,11 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command(["help"]))
 async def help_menu(bot, update):
-     text = f"Heya!\nTheese are the available commands right now\n\n/dog - sends a random dog image\n/cat - sends a random cat image\n/panda - sends a random panda image\n/meme - sends a random meme"
+     text = f"**Hey theese are the available commands:**\n"
+     text += f"➤ /start - To start Me:)\n"
+     text += f"➤ /help - To get this message\n"
+     text += f"➤ /dog - sends a random dog image\n"
+     text += f"➤ /cat - sends a random cat image\n"
+     text += f"➤ /panda - sends a random panda image\n"
+     text += f"➤ /meme - sends a random meme"
      await bot.send_message(message, text)
