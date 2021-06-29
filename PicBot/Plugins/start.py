3392c0c822b3d05061dff26bc702c7473bca7d20
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2021-06-29 15:08:51 +0530

    Update start.py

--- PicBot/Plugins/start.py
+++ PicBot/Plugins/start.py
@@ -1,10 +1,10 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+
 @Client.on_message(filters.command(["start"]))
 async def start(bot, update):
-    me = client.me.first_name
-    text=f"__Hello {me}!\nI'm Picturebot\nCheck__/help__ to get available commands"
+    text=f"__Hello!__\nI'm Picturebot\nCheck /help to get available commands"
     mypic="https://telegra.ph//file/894df85d26b8b72f0745b.jpg"
     await bot.send_photo(
             message.chat.id,
