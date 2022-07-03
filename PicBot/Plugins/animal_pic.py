35935ea4d70e39f8cde299b9925897ea10d77edb
Naveen_xd <85566702+Naveen-xd2580@users.noreply.github.com>
2022-07-03 11:50:23 +0530

    update animal_pic.py

--- PicBot/Plugins/animal_pic.py
+++ PicBot/Plugins/animal_pic.py
@@ -4,24 +4,24 @@ import re
 
         
 @Client.on_message(filters.command(["panda"]))
-async def panda(bot, update):
+async def panda(bot, message):
     link = "https://some-random-api.ml/img/panda"
     r = requests.get(url=link).json()
     image_s = r["link"]
-    await bot.send_photo(update.chat.id, image_s, reply_to_message_id=update.message_id)
+    await bot.send_photo(message.chat.id, image_s, reply_to_message_id=message.message_id)
 
 
 @Client.on_message(filters.command(["cat"]))
-async def cat(bot, update):
+async def cat(bot, message):
     link = "https://some-random-api.ml/img/cat"
     r = requests.get(url=link).json()
     image_s = r["link"]
-    await bot.send_photo(update.chat.id, image_s, reply_to_message_id=update.message_id)
+    await bot.send_photo(message.chat.id, image_s, reply_to_message_id=message.message_id)
 
 
 @Client.on_message(filters.command(["dog"]))
-async def dog(bot, update):
+async def dog(bot, message):
     link = "https://some-random-api.ml/img/dog"
     r = requests.get(url=link).json()
     image_s = r["link"]
-    await bot.send_photo(update.chat.id, image_s, reply_to_message_id=update.message_id)
+    await bot.send_photo(message.chat.id, image_s, reply_to_message_id=message.message_id)
