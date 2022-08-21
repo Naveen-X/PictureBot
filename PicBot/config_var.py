6dc3d100304d58c00af213248ed2b798cb5c1487
Naveen_xd <85566702+Naveen-X@users.noreply.github.com>
2022-08-21 08:48:08 +0530

    Update config_var.py

--- PicBot/config_var.py
+++ PicBot/config_var.py
@@ -7,26 +7,7 @@ from distutils.util import strtobool
 
 if os.path.exists("local.env"):
     load_dotenv("local.env")
-
-def fetch_heroku_git_url(api_key, app_name):
-    if not api_key:
-        return None
-    if not app_name:
-        return None
-    heroku = heroku3.from_key(api_key)
-    try:
-        heroku_applications = heroku.apps()
-    except:
-        return None
-    heroku_app = None
-    for app in heroku_applications:
-        if app.name == app_name:
-            heroku_app = app
-            break
-    if not heroku_app:
-        return None
-    return heroku_app.git_url.replace("https://", "https://api:" + api_key + "@")
-    
+     
 class Config(object):
     API_ID = int(os.environ.get("API_ID", 1))
     API_HASH = os.environ.get("API_HASH", None)
